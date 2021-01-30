c76acd7fc63abb5c7f8e9e18f78b18beeed26f3c
Luis Miguel Blanco <luismb55@gmail.com>
2021-01-30 13:27:39 +0100

    Organizes the folders hierarchy

---
+++ Assets/Scripts/MessageItemScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageItemScript : MonoBehaviour
+{
+    public enum MessageItemState
+    { 
+        Default,
+        WriteHighlight,
+        Written,
+        ReadHighlight,
+        ReadEmpty,
+        ReadMessage
+    }
+
+    private MessageItemState state;
+
+    public GameObject item;
+
+    public MessageItemState State
+    {
+        get { return state; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
