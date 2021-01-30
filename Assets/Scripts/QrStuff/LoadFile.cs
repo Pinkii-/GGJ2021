7b4e5b2751d9353177bc2be53a256b7c7731b0a9
Gonzalo Diez <gonzalod@gamehouse.com>
2021-01-30 15:54:03 +0100

    Load gameplay qr

--- Assets/Scripts/QrStuff/LoadFile.cs
+++ Assets/Scripts/QrStuff/LoadFile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 using ZXing;
 
@@ -8,34 +9,24 @@ namespace QrStuff
 {
     public class LoadFile : MonoBehaviour
     {
+        private string m_FilePath;
+        public UnityEvent<string> m_OnQrLoaded;
+ 
         [DllImport("__Internal")]
         private static extern void ImageUploaderCaptureClick();
-        public string FilePath;
-        //For Game Object where you want to apply the texture
-        public GameObject Player;
-        private Renderer renderer;
-        private int i=0;
- 
-        //  Call on Start
-        void Start()
-        {
-//  Set player object and textures
-//  In this case, I have assigned the tag "Player" to my game object of interest, where the texture is applied.
-            renderer = Player.GetComponent<Renderer>();
-        }
- 
+        
         public void OnButtonPointerDown () {
             //File browser when using Unity
 #if UNITY_EDITOR
-            FilePath = UnityEditor.EditorUtility.OpenFilePanel("Open image","","jpg,png");
-            if (!System.String.IsNullOrEmpty (FilePath))
-                FileSelected (FilePath);
+            m_FilePath = UnityEditor.EditorUtility.OpenFilePanel("Open image","","jpg,png");
+            if (!System.String.IsNullOrEmpty (m_FilePath))
+                FileSelected (m_FilePath);
 #else
     //File browser when using WebGL
     ImageUploaderCaptureClick ();
 #endif
         }
-        // Function reads your selected file and loads it as a texture, so it's ready to be used in a material.
+        
         void FileSelected (string url) {
             StartCoroutine(LoadTexture (url));
         }
@@ -48,17 +39,16 @@ namespace QrStuff
                 else {
                     //Apply texture to specific material slot
                     var texture = DownloadHandlerTexture.GetContent(www);
-                    renderer.materials[i].mainTexture = texture;
                 
                     IBarcodeReader barcodeReader = new BarcodeReader();
                     var result = barcodeReader.Decode(texture.GetPixels32(), texture.width, texture.height);
                 
                     if (result != null) {
-                        Debug.Log("DECODED TEXT FROM QR: " + result.Text);
+                        m_OnQrLoaded.Invoke(result.Text);
                     }
                     else
                     {
-                        Debug.Log("A iorar");
+                        Debug.Log("Not being able to read QR");
                     }
                 }
             }
