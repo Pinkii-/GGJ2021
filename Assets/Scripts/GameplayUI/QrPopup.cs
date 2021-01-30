3ebff64d84b6d9440ae7ae1da9f2ee19e8e80607
Gonzalo Diez <gonzalod@gamehouse.com>
2021-01-30 20:44:54 +0100

    Add some qr stuff to the gameplay

---
+++ Assets/Scripts/GameplayUI/QrPopup.cs
@@ -0,0 +1,25 @@
+﻿using QrStuff;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameplayUI
+{
+    public class QrPopup : MonoBehaviour
+    {
+        public Image m_QrVisualizer;
+        private static readonly int MaterialTexture = Shader.PropertyToID("QrTexture");
+
+        private void Start()
+        {
+            var password = GameManagerScript.gameManagerRef.GeneratePasswordFromMessages();
+            var qrTexture = GenerateQr.generateQR(password);
+
+            m_QrVisualizer.material.SetTexture(MaterialTexture, qrTexture);
+        }
+
+        public void DownloadQR()
+        {
+            WebGLFileSaver.SaveFile(GameManagerScript.gameManagerRef.GeneratePasswordFromMessages());
+        }
+    }
+}
