c0a467290712db4242e1252863b83edd731d9e7a
Gonzalo Diez <gonzalod@gamehouse.com>
2021-01-30 21:52:45 +0100

    Fix gameplay scene ui

--- Assets/Scripts/GameplayUI/MessageUI.cs
+++ Assets/Scripts/GameplayUI/MessageUI.cs
@@ -10,35 +10,35 @@ namespace GameplayUI
         [SerializeField] private TMP_InputField m_InputField;
 
         private Action<string> m_OnSaveButtonClicked;
+        private Action m_OnCancelButtonClicked;
 
-        public void InitAsWritableNote(string originalContent, Action<string> onSubmitButtonClicked)
+        public void InitAsWritableNote(string originalContent, Action<string> onSubmitButtonClicked, Action onCancelButtonClicked)
         {
             m_InputField.interactable = true;
             m_InputField.text = originalContent;
             m_OnSaveButtonClicked = onSubmitButtonClicked;
-            gameObject.SetActive(true);
+            m_OnCancelButtonClicked = onCancelButtonClicked;
             m_InputField.Select();
         }
 
-        public void InitAsReadableNote(string content, Action<string> onSubmitButtonClicked)
+        public void InitAsReadableNote(string content, Action<string> onSubmitButtonClicked, Action onCancelButtonClicked)
         {
             m_InputField.interactable = false;
             m_InputField.text = content;
             m_OnSaveButtonClicked = onSubmitButtonClicked;
-            gameObject.SetActive(true);
+            m_OnCancelButtonClicked = onCancelButtonClicked;
             m_InputField.Select();
         }
         
         public void OnCancelButtonPressed()
         {
             // TODO: Animation? sound?
-            gameObject.SetActive(false);
+            m_OnCancelButtonClicked?.Invoke();
         }
 
         public void OnSaveButtonPressed()
         {
             // TODO: Animation? sound?
-            gameObject.SetActive(false);
             
             m_OnSaveButtonClicked?.Invoke(m_InputField.text);
         }
