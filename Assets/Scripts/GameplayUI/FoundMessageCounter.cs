59a19b1c8dbae4227fe58a3bdae4c15d2b9a479f
Gonzalo Diez <gonzalod@gamehouse.com>
2021-01-31 01:18:48 +0100

    Add memories counter

---
+++ Assets/Scripts/GameplayUI/FoundMessageCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoundMessageCounter : MonoBehaviour
+{
+    [SerializeField] private GameObject m_MemoryElementPrefab;
+    
+    void Start()
+    {
+        m_MemoryElementPrefab.SetActive(false);
+    }
+
+    public void Refresh()
+    {
+        for (int i = 1; i < transform.childCount; ++i)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+
+        int totalMessageCount = GameManagerScript.gameManagerRef.GetTotalMessageCount();
+        int viewedMessages = GameManagerScript.gameManagerRef.GetViewedMessageCount();
+
+        for (int i = 0; i < totalMessageCount; ++i)
+        {
+            var go = Instantiate(m_MemoryElementPrefab, transform);
+            go.SetActive(true);
+            if (i < viewedMessages) go.transform.Find("Full Slot").gameObject.SetActive(true);
+            else go.transform.Find("Empty Slot").gameObject.SetActive(true);
+        }
+    }
+}
