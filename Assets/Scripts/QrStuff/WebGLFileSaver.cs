ae73e9f8a3e0a1568316fdcb96b91d49f7cb13ae
Gonzalo Diez <gonzalod@gamehouse.com>
2021-02-03 20:25:47 +0100

    Save file in non webgl

--- Assets/Scripts/QrStuff/WebGLFileSaver.cs
+++ Assets/Scripts/QrStuff/WebGLFileSaver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using SFB;
 using UnityEngine;
 
 namespace QrStuff
@@ -14,74 +16,27 @@ namespace QrStuff
             if (String.IsNullOrEmpty(text)) return;
 
             var qr = GenerateQr.generateQR(text);
-        
-            var encodedText = System.Convert.ToBase64String(qr.EncodeToPNG());
-        
-            var image_url = "data:image/png;base64," + encodedText;
-        
-            SaveScreenshotWebGL("memories.png", image_url);
+
+            var pngBytes = qr.EncodeToPNG();
+            
+            
+            if (Application.platform != RuntimePlatform.WebGLPlayer)
+            {
+                var extensionList = new [] {
+                    new ExtensionFilter("Image", "png"),
+                };
+                var path = StandaloneFileBrowser.SaveFilePanel("Save File", "", "memories", extensionList);
+                File.WriteAllBytes(path, pngBytes);
+            }
+            else
+            {
+                var encodedText = System.Convert.ToBase64String(pngBytes);
+                var image_url = "data:image/png;base64," + encodedText;
+                SaveScreenshotWebGL("memories.png", image_url);
+            }
         }
     
         [DllImport("__Internal")]
         private static extern void SaveScreenshotWebGL(string filename, string data);
-
-        [DllImport("__Internal")]
-        private static extern void UNITY_SAVE(string content, string name, string MIMEType);
-
-        [DllImport("__Internal")]
-        private static extern void init();
-
-        [DllImport("__Internal")]
-        private static extern bool UNITY_IS_SUPPORTED();
-
-        static bool hasinit = false;
-
-        public static void SaveFile(string content, string fileName, string MIMEType = "text/plain;charset=utf-8")
-        {
-            if (Application.isEditor)
-            {
-                Debug.Log("Saving will not work in editor.");
-                return;
-            }
-            if (Application.platform != RuntimePlatform.WebGLPlayer)
-            {
-                Debug.Log("Saving must be on a WebGL build.");
-                return;
-            }
-
-            CheckInit();
-
-            if (!IsSavingSupported())
-            { 
-                Debug.LogWarning("Saving is not supported on this device.");
-                return;
-            }
-            UNITY_SAVE(content, fileName, MIMEType);
-        }
-
-        static void CheckInit()
-        {
-            if (!hasinit)
-            {
-                init();
-                hasinit = true;
-            }
-        }
-
-        public static bool IsSavingSupported()
-        {
-            if (Application.isEditor)
-            {
-                Debug.Log("Saving will not work in editor.");
-                return false;
-            }
-            if (Application.platform != RuntimePlatform.WebGLPlayer)
-            {
-                Debug.Log("Saving must be on a WebGL build.");
-                return false;
-            }
-            CheckInit();
-            return UNITY_IS_SUPPORTED();
-        }
     }
 }
